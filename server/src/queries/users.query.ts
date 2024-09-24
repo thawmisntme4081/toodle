b38927c2ff7352f1896b23f4036dab0a38d6f6a0
Billy N <ntuanthanh184@gmail.com>
2024-09-24 21:10:22 +0700

    refactor: remove server

--- server/src/queries/users.query.ts
+++
@@ -1,5 +0,0 @@
-export const COUNT_USERS = 'SELECT COUNT(*) FROM users'
-export const INSERT_USER =
-  'INSERT INTO users (firstName, lastName, email, password, role) VALUES ($1, $2, $3, $4, $5)'
-export const CHECK_USER =
-  'SELECT id, password, role FROM users WHERE email = $1'
