7b09ad25804a501179315d94d8fe8d7ebe0af317
Billy N <tiennx@vmogroup.com>
2024-10-15 17:36:41 +0700

    refactor: UI layout

--- src/containers/Navbar/HorizontalNavBar.tsx
+++ src/containers/Navbar/HorizontalNavBar.tsx
@@ -1,18 +1,25 @@
+import { useSelector } from 'react-redux'
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { IconBell, IconSun } from '@/icons'
+import { RootState } from '@/redux/store'
 
 import MobileNavbar from './MobileNavbar'
 
 const HorizontalNavbar = () => {
+  const fallBackAvatar = useSelector(
+    (state: RootState) => state.auth.fallBackAvatar,
+  )
+
   return (
-    <div className="flex justify-between bg-white rounded items-center md:flex-row-reverse">
+    <div className="flex justify-between bg-white rounded items-center lg:flex-row-reverse">
       <MobileNavbar />
       <div className="p-2 flex items-center gap-4">
         <IconSun className="w-5 h-5" />
         <IconBell className="w-5 h-5" />
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarImage src="" />
+          <AvatarFallback>{fallBackAvatar}</AvatarFallback>
         </Avatar>
       </div>
     </div>
