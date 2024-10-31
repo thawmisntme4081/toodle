db912510c26b575649561d4eb30ba7bdd629f234
QDuy <nguyenquocduy0207@gmail.com>
2024-10-31 11:44:39 +0700

    feat: Add styles for big calendar and integrate it into TeacherDetailPage

--- src/containers/Teachers/TeacherDetailPage.tsx
+++ src/containers/Teachers/TeacherDetailPage.tsx
@@ -1,9 +1,43 @@
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
 import { Route } from '@/routes/_authenticated/teachers/$teacherId.lazy'
+
+import '@/big-calendar.css'
+
+const events = [
+  {
+    start: moment().toDate(),
+    end: moment().add(1, 'days').toDate(),
+    title: 'Some title',
+    color: 'red',
+  },
+]
+
+const localizer = momentLocalizer(moment)
 
 const TeacherDetailPage = () => {
   const { teacherId } = Route.useParams()
 
-  return <div>{`TeacherDetailPage ${teacherId}`}</div>
+  return (
+    <>
+      <div>{`TeacherDetailPage ${teacherId}`}</div>
+      <div>
+        <BigCalendar
+          localizer={localizer}
+          startAccessor="start"
+          endAccessor="end"
+          events={events}
+          style={{ height: 500 }}
+          eventPropGetter={(event) => ({
+            style: {
+              backgroundColor: event.color,
+            },
+          })}
+        />
+      </div>
+    </>
+  )
 }
 
 export default TeacherDetailPage
